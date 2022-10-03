75c36663817f088925701ee5836c3cd1aa7e2533
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2022-10-03 14:09:05 +0500

    Interceptor added to handle client side errors

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private route:Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(error=>{
+        if(error){
+          if(error.status===404)
+          {
+            this.route.navigateByUrl('/not-found');
+          }
+          if(error.status===500){
+            this.route.navigateByUrl('/server-error')
+          }
+        }
+        return throwError(error)
+      })
+    );
+  }
+}
