25469b3468995d73168c6ae974b979a2cae8c50f
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2022-10-08 22:54:35 +0500

    breadcrumbs added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'test-error',component:TestErrorComponent},
-  {path:'not-found',component:NotFoundError},
-  {path:'server-error',component:ServerErrorComponent},
-  {path:'shop',loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule)},
+  {path:'',component:HomeComponent,data:{breadcrumb:'Home'}},
+  {path:'test-error',component:TestErrorComponent,data:{breadcrumb:'Test Errors'}},
+  {path:'not-found',component:NotFoundError,data:{breadcrumb:'Not Found'}},
+  {path:'server-error',component:ServerErrorComponent,data:{breadcrumb:'Server error'}},
+  {path:'shop',loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule),data:{breadcrumb:'Shop'}},
   {path:'**',redirectTo:'',pathMatch:'full'}
 ];
 
