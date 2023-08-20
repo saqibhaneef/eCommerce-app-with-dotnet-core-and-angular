c4cb0c28dd667f3b7e9e5f9a38af651b702a8d13
saqibhaneef <muhammadsaqibhanif80@gmail.com>
2023-08-20 15:05:08 +0500

    Revert "Revert "section:17: Identity client login added""
    This reverts commit 921b65857bdd4d053d86a3078ec3649e3e489477.

---
+++ client/src/app/account/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent{
+
+  loginForm=new FormGroup({
+    email:new FormControl('',Validators.required),
+    password:new FormControl('',Validators.required)
+  })
+
+  constructor(private accountService:AccountService) { }
+
+  ngOnInit(): void {
+  }
+  
+
+  onSubmit(){
+    this.accountService.login(this.loginForm.value).subscribe({
+      next:user=>{
+        console.log(user)        
+      }  
+    });
+    
+    //console.log(this.loginForm.value)
+  }
+
+}
